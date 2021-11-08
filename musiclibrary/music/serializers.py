2690eea05af363dd78d83def8b982152b82b042c
Brycen Harmon <brycenharmon@gmail.com>
2021-11-08 08:25:25 -0600

    finished the template fro most of the assignemt, could not get it to work

--- musiclibrary/music/serializers.py
+++ musiclibrary/music/serializers.py
@@ -5,3 +5,15 @@ class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
         fields = ['id', 'title', 'artist', 'album', 'genre', 'release_date']
+
+    def create(self, validated_data):
+        return Song.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.artist = validated_data.get('artist', instance.artist)
+        instance.album = validated_data.get('album', instance.album)
+        instance.genre = validated_data.get('genre', instance.genre)
+        instance.release_date = validated_data.get('release_date', instance.release_date)
+        instance.save()
+        return instance
--- musiclibrary/music/urls.py
+++ musiclibrary/music/urls.py
@@ -4,5 +4,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('music/', views.SongList.as_view())
+    path('music/', views.SongList.as_view()),
+    path('music/<int:pk>/', views.SongDetail.as_view())
 ]
